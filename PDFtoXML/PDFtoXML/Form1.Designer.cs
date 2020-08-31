861b272d00961f1ef170339706ec64ccfc5e0251
Thiago Teixeira <62726137+xthiagoteixeira@users.noreply.github.com>
2020-08-31 13:47:21 -0300

    Exportar arquivo PDF para XML
    Este exemplo é para exportar arquivo PDF para XML

---
+++ PDFtoXML/PDFtoXML/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace PDFtoXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.openFD = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(73, 37);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(321, 20);
+            this.textEdit1.TabIndex = 0;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(26, 40);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(41, 13);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "Arquivo:";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.AutoSize = true;
+            this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.simpleButton1.Location = new System.Drawing.Point(409, 36);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(24, 22);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
+            this.simpleButton2.Location = new System.Drawing.Point(319, 63);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton2.TabIndex = 3;
+            this.simpleButton2.Text = "Gerar!";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // openFD
+            // 
+            this.openFD.FileName = "openFileDialog1";
+            this.openFD.FileOk += new System.ComponentModel.CancelEventHandler(this.openFD_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 93);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.textEdit1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private System.Windows.Forms.OpenFileDialog openFD;
+    }
+}
+
---
+++ PDFtoXML/PDFtoXML/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.Pdf;
+
+namespace PDFtoXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            // Abrir Caixa de Dialogo
+            // Abre direto no desktop...
+            openFD.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFD.Title = "Abrir arquivo";
+            openFD.Filter = "Arquivo PDF|*.pdf";
+            openFD.FilterIndex = 1;
+            openFD.ShowDialog();
+            textEdit1.Text = openFD.FileName;
+
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+
+
+            using (PdfDocumentProcessor processor = new PdfDocumentProcessor())
+            {
+
+                // Load a PDF document with AcroForm data. 
+                processor.LoadDocument(textEdit1.Text);
+
+                // Substitui pra XML o Destino com mesmo nome do arquivo.
+                string FileXML = textEdit1.Text;
+                FileXML = textEdit1.Text.Replace(".pdf", ".xml");
+
+                // Export AcroForm data to XML format.
+                processor.Export(FileXML, PdfFormDataFormat.Xml);
+            }
+
+
+
+        }
+
+        private void openFD_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
